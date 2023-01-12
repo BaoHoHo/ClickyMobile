6c4678ccb5a87f86efc181443d00bc3e9876ad69
BaoHoHo <96350422+BaoHoHo@users.noreply.github.com>
2023-01-12 16:25:34 -0500

    push all

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("con heo");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
